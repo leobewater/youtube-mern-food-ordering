657684b363c90a0d2855d86c360de871720a6606
leobewater <mmbee888@gmail.com>
2024-02-20 16:52:58 -0500

    clean up unused import

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,5 +1,4 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
@@ -16,7 +15,7 @@ export const Auth0ProviderWithNavigate = ({ children }: Props) => {
     throw new Error("unable to initialize auth");
   }
 
-  const onRedirectCallback = (appState?: AppState, user?: User) => {
+  const onRedirectCallback = () => {
     // console.log("USER", user);
     navigate("/auth-callback");
   };

6085aaaab2074598bed351fa08c90af99183b05d
leobewater <mmbee888@gmail.com>
2024-02-20 10:21:20 -0500

    add header content

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 export const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="border-b-2 border-b-orange-500 py-6">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link
+          to="/"
+          className="text-3xl font-bold tracking-tight text-orange-500"
+        >
+          MernEats.com
+        </Link>
+      </div>
+    </div>
+  );
+};

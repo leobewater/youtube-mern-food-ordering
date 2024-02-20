735697a53548217a0c21f6c6dcedb8b78327f87f
leobewater <mmbee888@gmail.com>
2024-02-20 10:09:22 -0500

    add approutes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "@/AppRoutes.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AppRoutes />
     </Router>
   </React.StrictMode>
 );

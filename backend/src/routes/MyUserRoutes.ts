5851eed42be1fb7218db71cbd9e75ac0b1e974e5
leobewater <mmbee888@gmail.com>
2024-02-21 06:34:40 -0500

    add validate request middleware

--- backend/src/routes/MyUserRoutes.ts
+++ backend/src/routes/MyUserRoutes.ts
@@ -2,11 +2,18 @@ import express from "express";
 import myUserController from "../controllers/MyUserController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
 import MyUserController from "../controllers/MyUserController";
+import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
 // /api/my/user with middleware
 router.post("/", jwtCheck, myUserController.createCurrentUser);
-router.put("/", jwtCheck, jwtParse, MyUserController.updateCurrentUser);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateMyUserRequest,
+  MyUserController.updateCurrentUser
+);
 
 export default router;

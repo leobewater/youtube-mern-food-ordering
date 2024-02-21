a9ff6616ab21cbe7ac736ded2a23bfcd09f53edf
leobewater <mmbee888@gmail.com>
2024-02-21 06:26:16 -0500

    add user put endpoint

--- backend/src/routes/MyUserRoutes.ts
+++ backend/src/routes/MyUserRoutes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import myUserController from "../controllers/MyUserController";
 import { jwtCheck } from "../middleware/auth";
+import MyUserController from "../controllers/MyUserController";
 
 const router = express.Router();
 
 // /api/my/user with middleware
 router.post("/", jwtCheck, myUserController.createCurrentUser);
+router.put("/", MyUserController.updateCurrentUser)
 
 export default router;

06b9fcd96edc06ee386288f6c61dc62aaad9d65e
leobewater <mmbee888@gmail.com>
2024-02-21 06:27:16 -0500

    add jwtParse to user put endpoint

--- backend/src/routes/MyUserRoutes.ts
+++ backend/src/routes/MyUserRoutes.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import myUserController from "../controllers/MyUserController";
-import { jwtCheck } from "../middleware/auth";
+import { jwtCheck, jwtParse } from "../middleware/auth";
 import MyUserController from "../controllers/MyUserController";
 
 const router = express.Router();
 
 // /api/my/user with middleware
 router.post("/", jwtCheck, myUserController.createCurrentUser);
-router.put("/", MyUserController.updateCurrentUser)
+router.put("/", jwtCheck, jwtParse, MyUserController.updateCurrentUser);
 
 export default router;

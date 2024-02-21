251310617372774011b41ff8feb7ae28e5763994
leobewater <mmbee888@gmail.com>
2024-02-21 10:30:10 -0500

    add sonner

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,6 +1,7 @@
-import { Layout } from "@/layouts/layout";
+import { Layout } from "@/layouts/Layout";
 import { AuthCallbackPage } from "@/pages/AuthCallbackPage";
 import { HomePage } from "@/pages/Homepage";
+import { UserProfilePage } from "@/pages/UserProfilePage";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 const AppRoutes = () => {
@@ -9,13 +10,20 @@ const AppRoutes = () => {
       <Route
         path="/"
         element={
-          <Layout>
+          <Layout showHero>
             <HomePage />
           </Layout>
         }
       />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
-      <Route path="/user-profile" element={<span>User Profile Page</span>} />
+      <Route
+        path="/user-profile"
+        element={
+          <Layout showHero={false}>
+            <UserProfilePage />
+          </Layout>
+        }
+      />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );

5342665b88ef9c0d29f4ad3247a1f951452d48ed
leobewater <mmbee888@gmail.com>
2024-02-21 14:31:39 -0500

    fix duplicated import

--- backend/src/routes/MyUserRoutes.ts
+++ backend/src/routes/MyUserRoutes.ts
@@ -1,14 +1,13 @@
 import express from "express";
-import myUserController from "../controllers/MyUserController";
-import { jwtCheck, jwtParse } from "../middleware/auth";
 import MyUserController from "../controllers/MyUserController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
 import { validateMyUserRequest } from "../middleware/validation";
 
 const router = express.Router();
 
 // /api/my/user with middleware
-router.get("/", jwtCheck, jwtParse, myUserController.getCurrentUser);
-router.post("/", jwtCheck, myUserController.createCurrentUser);
+router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
 router.put(
   "/",
   jwtCheck,

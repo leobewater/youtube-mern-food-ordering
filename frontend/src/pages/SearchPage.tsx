647fbf8b77dea2c656b2ed586e5ecc663bd6a04a
leobewater <mmbee888@gmail.com>
2024-02-22 14:23:40 -0500

    add search result info

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,20 +1,30 @@
 import { useSearchRestaurants } from "@/api/RestaurantApi";
+import { SearchBar } from "@/components/SearchBar";
+import { SearchResultInfo } from "@/components/SearchResultInfo";
 import { useParams } from "react-router-dom";
 
 export default function SearchPage() {
   const { city } = useParams();
-  const { results } = useSearchRestaurants(city);
+  const { results, isLoading } = useSearchRestaurants(city);
+
+  if (isLoading) {
+    <span>Loading...</span>;
+  }
+
+  if (!results?.data || !city) {
+    return <span>No results found</span>;
+  }
 
   return (
-    <div>
-      User search for {city}{" "}
-      <span>
-        {results?.data.map((restaurant) => (
-          <div>
-            found - {restaurant.restaurantName}, {restaurant.city}
-          </div>
-        ))}
-      </span>
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div id="cuisines-list">Insert cuisines here</div>
+      <div id="main-content" className="flex flex-col gap-5">
+        <SearchBar
+          onSubmit={() => {}}
+          placeHolder="Search by Cuisine or Restaurant Name"
+        />
+        <SearchResultInfo total={results.pagination.total} city={city} />
+      </div>
     </div>
   );
 }

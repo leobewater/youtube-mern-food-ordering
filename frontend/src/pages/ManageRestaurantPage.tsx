8a26faec37cdef16116df462e558cc6645c6d8c9
leobewater <mmbee888@gmail.com>
2024-02-22 09:24:03 -0500

    add create restaurant api

--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,5 +1,10 @@
+import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
 import { ManageRestaurantForm } from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 export const ManageRestaurantPage = () => {
-  return <ManageRestaurantForm onSave={() => {}} isLoading={false} />;
+  const { createRestaurant, isLoading } = useCreateMyRestaurant();
+
+  return (
+    <ManageRestaurantForm onSave={createRestaurant} isLoading={isLoading} />
+  );
 };

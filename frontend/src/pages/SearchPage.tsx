70c959a075b6ff9a1ce0800d1a1f4f587092c548
leobewater <mmbee888@gmail.com>
2024-02-22 14:12:28 -0500

    populate search results

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,6 +1,20 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
 import { useParams } from "react-router-dom";
 
 export default function SearchPage() {
   const { city } = useParams();
-  return <div>User search for {city}</div>;
+  const { results } = useSearchRestaurants(city);
+
+  return (
+    <div>
+      User search for {city}{" "}
+      <span>
+        {results?.data.map((restaurant) => (
+          <div>
+            found - {restaurant.restaurantName}, {restaurant.city}
+          </div>
+        ))}
+      </span>
+    </div>
+  );
 }

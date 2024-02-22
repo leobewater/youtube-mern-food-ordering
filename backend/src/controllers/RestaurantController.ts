b071000e76bf0ef134e640080a365fe5fa209d98
leobewater <mmbee888@gmail.com>
2024-02-22 11:22:55 -0500

    add restaurant search route on BE

---
+++ backend/src/controllers/RestaurantController.ts
@@ -0,0 +1,3 @@
+const searchRestaurant = async () => {};
+
+export { searchRestaurant };
---
+++ backend/src/routes/RestaurantRoute.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { param } from "express-validator";
+
+const router = express.Router();
+
+router.get(
+  "/search/:city",
+  param("city")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("City parameter must be a valid string"),
+  RestaurantController.searchRestaurants
+);

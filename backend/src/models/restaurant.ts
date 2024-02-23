ef72c2929cadacc803945c8a22044a1b4a28bfcd
leobewater <mmbee888@gmail.com>
2024-02-23 10:05:38 -0500

    export menuitem type in BE

--- backend/src/models/restaurant.ts
+++ backend/src/models/restaurant.ts
@@ -1,9 +1,11 @@
-import mongoose from "mongoose";
+import mongoose, { InferSchemaType } from "mongoose";
 
 const menuItemSchema = new mongoose.Schema({
+  _id: { type: mongoose.Schema.Types.ObjectId, required: true },
   name: { type: String, required: true },
   price: { type: Number, required: true },
 });
+export type MenuItemType = InferSchemaType<typeof menuItemSchema>;
 
 const restuarantSchema = new mongoose.Schema({
   // user document reference

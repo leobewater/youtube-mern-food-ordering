9ac37a12d14bc1f48e83d2c3e5c876c754a7e24a
leobewater <mmbee888@gmail.com>
2024-02-23 13:51:42 -0500

    add orders to manage restaurant page

---
+++ frontend/src/components/OrderItemCard.tsx
@@ -0,0 +1,13 @@
+import { Order } from "@/types";
+
+type Props = {
+  order: Order;
+};
+
+export const OrderItemCard = ({ order }: Props) => {
+  return (
+    <div className="space-y-5">
+Order Card
+    </div>
+  );
+};

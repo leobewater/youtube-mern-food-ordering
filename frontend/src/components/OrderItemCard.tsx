6ab2b04d665980bd9ab6d61b856ec7fd38970d9c
leobewater <mmbee888@gmail.com>
2024-02-23 13:56:49 -0500

    show order details

--- frontend/src/components/OrderItemCard.tsx
+++ frontend/src/components/OrderItemCard.tsx
@@ -1,3 +1,4 @@
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { Order } from "@/types";
 
 type Props = {
@@ -5,9 +6,44 @@ type Props = {
 };
 
 export const OrderItemCard = ({ order }: Props) => {
+  const getTime = () => {
+    const orderDateTime = new Date(order.createdAt);
+
+    const hours = orderDateTime.getHours();
+    const minutes = orderDateTime.getMinutes();
+    const paddedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+
+    return `${hours}:${paddedMinutes}`;
+  };
+
   return (
-    <div className="space-y-5">
-Order Card
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle className="grid md:grid-cols-4 gap-4 justify-between mb-3">
+          <div>
+            Customer Name:
+            <span className="ml-2 font-normal">
+              {order.deliveryDetails.name}
+            </span>
+          </div>
+          <div>
+            Delivery Address:
+            <span className="ml-2 font-normal">
+              {order.deliveryDetails.addressLine1}, {order.deliveryDetails.city}
+            </span>
+          </div>
+          <div>
+            Time:
+            <span className="ml-2 font-normal">{getTime()}</span>
+          </div>
+          <div>
+            Total Cost:
+            <span className="ml-2 font-normal">
+              ${(order.totalAmount / 100).toFixed(2)}
+            </span>
+          </div>
+        </CardTitle>
+      </CardHeader>
+    </Card>
   );
 };
